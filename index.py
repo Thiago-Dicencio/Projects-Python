e39704b81594c7b1dab60a201c4ae05525891d47
Thiago Castro Dicencio <136647457+Thiago-Dicencio@users.noreply.github.com>
2024-03-25 11:28:06 -0300

    prime commit

---
+++ index.py
@@ -0,0 +1,8 @@
+def soma(a, b):
+    return a + b
+
+nun1 = float(input("Digite o primeiro número: 10"))
+nun2 = float(input("Digite o segundo número: 10"))
+
+resultado = soma(nun1, nun2)
+print("A soma é:", resultado)
